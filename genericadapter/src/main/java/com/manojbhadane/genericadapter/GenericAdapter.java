fda1c6ac81a6340305c61ccf84f405d81e4782e5
Ivan from Manigo <ivan@manigo.com>
2020-10-28 00:04:39 +0100

    merged everything into 1 adapter

--- genericadapter/src/main/java/com/manojbhadane/genericadapter/GenericAdapter.java
+++ genericadapter/src/main/java/com/manojbhadane/genericadapter/GenericAdapter.java
@@ -2,6 +2,9 @@ package com.manojbhadane.genericadapter;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
@@ -10,18 +13,25 @@ import androidx.databinding.ViewDataBinding;
 import androidx.databinding.library.baseAdapters.BR;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.jakewharton.rxbinding4.widget.RxTextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.rxjava3.disposables.Disposable;
+
 /**
  * @author manoj.bhadane manojbhadane777@gmail.com
- * edited by ivambass1@gmail.com
+ * edited by ivankostadinovic1994@outlook.com
  */
 
-public abstract class GenericAdapter<T, D extends ViewDataBinding> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public abstract class GenericAdapter<T, D extends ViewDataBinding> extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
 
     private List<T> mArrayList;
+    private List<T> mArrayListFilter;
     private int layoutResId;
+    private EditText searchView;
+    private Disposable searchDisposable;
 
     public GenericAdapter(List<T> arrayList, @LayoutRes int layoutResId) {
         this.mArrayList = new ArrayList<>();
@@ -86,7 +96,6 @@ public abstract class GenericAdapter<T, D extends ViewDataBinding> extends Recyc
         return mArrayList.get(position);
     }
 
-
     private class ItemViewHolder extends RecyclerView.ViewHolder {
         private D mDataBinding;
 
@@ -94,5 +103,62 @@ public abstract class GenericAdapter<T, D extends ViewDataBinding> extends Recyc
             super(binding.getRoot());
             mDataBinding = (D) binding;
         }
+    }
+
+    public GenericAdapter(List<T> arrayList, @LayoutRes int layoutResId, EditText searchView) {
+        this.mArrayList = new ArrayList<>();
+        this.mArrayList.addAll(arrayList);
+        this.layoutResId = layoutResId;
+        this.searchView = searchView;
+
+        searchDisposable = RxTextView
+            .textChanges(this.searchView)
+            .subscribe(text -> getFilter().filter(text));
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(@NonNull RecyclerView recyclerView) {
+        if (searchDisposable != null) {
+            searchDisposable.dispose();
+        }
+        super.onDetachedFromRecyclerView(recyclerView);
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+
+                List<T> arrayList = new ArrayList<>();
+
+                try {
+                    if (charSequence.toString().isEmpty()) {
+                        arrayList = mArrayList;
+                    } else {
+                        arrayList = performFilter(charSequence.toString().trim(), mArrayList);
+                        if (arrayList == null) {
+                            arrayList = mArrayList;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = arrayList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mArrayListFilter = (ArrayList<T>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public List<T> performFilter(String text, List<T> originalList) {
+        return null;
     }
 }
